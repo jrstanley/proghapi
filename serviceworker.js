0c1c09ad7007a237dd18b9d890bed491e31a8123
James <jrstanley@users.noreply.github.com>
2017-03-28 13:54:29 +0100

    Adds BASE_PATH

--- serviceworker.js
+++ serviceworker.js
@@ -1,51 +1,53 @@
+var BASE_PATH = '/staffs-201617-proghapi/';
 var CACHE_NAME = 'gih-cache-v5';
 var CACHED_URLS = [
-  // Our HTML
-  'first.html',
-  // Images for favicons
-    'appimages/android-icon-36x36.png',
-    'appimages/android-icon-48x48.png',
-    'appimages/android-icon-72x72.png',
-    'appimages/android-icon-96x96.png',
-    'appimages/android-icon-144x144.png',
-    'appimages/android-icon-192x192.png',
-    'appimages/favicon-32x32.png',
-    //Images for page
+    // Our HTML
+    BASE_PATH + 'first.html',
     
-    'appimages/dino.png',
-  'appimages/jack.jpg',
-  'appimages/paddy.jpg',
-    'appimages/favicon.ico',
-  'appimages/favicon-16x16.png',
-  'appimages/favicon-32x32.png',
-  'appimages/favicon-96x96.png',
-  'appimages/ms-icon-70x70.png',
-  'appimages/ms-icon-144x144.png',
-  'appimages/ms-icon-150x150.png',
-  'appimages/ms-icon-310x310.png',
-  'eventsimages/example-blog01.jpg',
-  'eventsimages/example-blog02.jpg',
-  'eventsimages/example-blog03.jpg',
-  'eventsimages/example-blog04.jpg',
-  'eventsimages/example-blog05.jpg',
-  'eventsimages/example-blog06.jpg',
-  'eventsimages/example-blog07.jpg',
-  'eventsimages/example-work01.jpg',
-  'eventsimages/example-work02.jpg',
-  'eventsimages/example-work03.jpg',
-  'eventsimages/example-work04.jpg',
-  'eventsimages/example-work05.jpg',
-  'eventsimages/example-work06.jpg',
-  'eventsimages/example-work07.jpg',
-  'eventsimages/example-work08.jpg',
-  'eventsimages/example-work09.jpg',  
-  // JavaScript
-    'material.js',
+    // Images for favicons
+    BASE_PATH + 'appimages/android-icon-36x36.png',
+    BASE_PATH + 'appimages/android-icon-48x48.png',
+    BASE_PATH + 'appimages/android-icon-72x72.png',
+    BASE_PATH + 'appimages/android-icon-96x96.png',
+    BASE_PATH + 'appimages/android-icon-144x144.png',
+    BASE_PATH + 'appimages/android-icon-192x192.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+
+    //Images for page
+    BASE_PATH + 'appimages/dino.png',
+    BASE_PATH + 'appimages/jack.jpg',
+    BASE_PATH + 'appimages/paddy.jpg',
+    BASE_PATH + 'appimages/favicon.ico',
+    BASE_PATH + 'appimages/favicon-16x16.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+    BASE_PATH + 'appimages/favicon-96x96.png',
+    BASE_PATH + 'appimages/ms-icon-70x70.png',
+    BASE_PATH + 'appimages/ms-icon-144x144.png',
+    BASE_PATH + 'appimages/ms-icon-150x150.png',
+    BASE_PATH + 'appimages/ms-icon-310x310.png',
+    BASE_PATH + 'eventsimages/example-blog01.jpg',
+    BASE_PATH + 'eventsimages/example-blog02.jpg',
+    BASE_PATH + 'eventsimages/example-blog03.jpg',
+    BASE_PATH + 'eventsimages/example-blog04.jpg',
+    BASE_PATH + 'eventsimages/example-blog05.jpg',
+    BASE_PATH + 'eventsimages/example-blog06.jpg',
+    BASE_PATH + 'eventsimages/example-blog07.jpg',
+    BASE_PATH + 'eventsimages/example-work01.jpg',
+    BASE_PATH + 'eventsimages/example-work02.jpg',
+    BASE_PATH + 'eventsimages/example-work03.jpg',
+    BASE_PATH + 'eventsimages/example-work04.jpg',
+    BASE_PATH + 'eventsimages/example-work05.jpg',
+    BASE_PATH + 'eventsimages/example-work06.jpg',
+    BASE_PATH + 'eventsimages/example-work07.jpg',
+    BASE_PATH + 'eventsimages/example-work08.jpg',
+    BASE_PATH + 'eventsimages/example-work09.jpg',  
+    // JavaScript
+    BASE_PATH + 'material.js',
   // CSS and fonts
     'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'min-style.css',
-    'styles.css'
+    BASE_PATH + 'min-style.css',
+    BASE_PATH + 'styles.css'
 ];
 
 var googleMapsAPIJS = 'YOUR_GOOGLE_MAPURL';
